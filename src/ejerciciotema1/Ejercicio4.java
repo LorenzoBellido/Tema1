d9f10a7bb7510e8ffb71fd36df300d91bb11b1e8
ljbellido <ljbellido@106-07.info2>
2023-09-27 10:11:11 +0200

    Ejercicios 1

---
+++ src/ejerciciotema1/Ejercicio4.java
@@ -0,0 +1,19 @@
+package ejerciciotema1;
+
+import java.util.Scanner;
+
+public class Ejercicio4 {
+
+	public static void main(String[] args) {
+		// Variable donde guardar el área
+		float base;
+		
+		// Variable donde guardar la altura
+		float altura;
+		
+		// Scanner para leer el teclado
+		Scanner sc = new Scanner(System.in);
+
+	}
+
+}
